1d817e7b74f360727b30218659e0fa8364a4f2e9
Sydney Renee <sydney@solace.ofharmony.ai>
2026-05-22 11:35:15 -0700

    chore: remove mavenLocal()

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,7 +11,6 @@ plugins { id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
 
 dependencyResolutionManagement {
     repositories {
-        mavenLocal()
         google()
         mavenCentral()
     }
